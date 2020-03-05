8474ef4b067a8b26836a25796571f54c553e530d
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-03-05 23:00:07 +0700

    Update grabspams.py

--- grabspams.py
+++ grabspams.py
@@ -5,6 +5,7 @@
 # Github: https://github.com/stepbystepexe/Grabspams
 
 import os, sys, time, random
+from time import sleep
 
 info = """
 Nama        : Grabspams
@@ -24,7 +25,7 @@ NB          : Manusia gax ada yang sempurna
 
 [ \033[4mGunakan tool ini dengan bijak \033[0m]\n """
 
-example = """\033[0;101;77;1m[        Grabspams OTP, My Github: @stepbystepexe        ]\033[0m"""
+example = """\033[0;101;77;1m[          Grabspams, My Github: @stepbystepexe          ]\033[0m"""
 
 logo = """
  \033[0;36m█▀▀▀▀ █▀▀▀▄ █▀▀▀█ █▀▀▀▄ █▀▀▀▀ █▀▀▀█ █▀▀▀█ █▀▀█▀▀█ █▀▀▀▀
@@ -43,25 +44,37 @@ def write(o):
         sys.stdout.flush()
         time.sleep(0.03)
 
+def serverotp():
+    os.system('clear')
+    os.system('reset')
+    sleep(1)
+    print()
+    print(logo)
+    print(example)
+    print()
+    write("\033[0m[ \033[32mINFO \033[0m] \033[3mSaya tidak real kalo code program saya ini ditiru")
+    write("         Seburuk apaun itu tolong hargai karya milik orang\033[0m\n")
+    os.system('php .otp')
+
 os.system('clear')
 os.system('reset')
-time.sleep(1)
+sleep(1)
 print()
 print(logo)
 print(example)
 print()
-print("\033[0m[\033[1;96mあ\033[0m] \033[1;77mKirim Spam OTP")
+print("\033[0m[\033[1;96;2m1\033[0m] \033[1;77mKirim Spam OTP")
 print()
 print("\033[0m[\033[93;1m&\033[0m] LISENSI")
 print("\033[0m[\033[94;1m#\033[0m] Informasi")
 print("\033[0m[\033[92;1m*\033[0m] Perbarui")
 print("\033[0m[\033[91;1m-\033[0m] Keluar")
 print()
-option = input("\033[0m[\033[1;95m/\033[0m] \033[1;77mMasukan opsi: \033[0m")
-if option.strip() in 'あ 1 kirim'.split():
-    write("\n\n\033[0m[\033[91;1m!\033[0m] Membuka Server OTP ...\033[0m\n")
-    time.sleep(1)
-    os.system('php __otp__.php')
+option = input("\033[0m(\033[105;77;1m/\033[0m) \033[1;77mMasukan opsi: \033[0m")
+if option.strip() in '1 kirim'.split():
+    write("\n\033[0m[\033[32m●\033[0m] \033[77;1mSedang memproses ...\033[0m")
+    sleep(1)
+    serverotp()
 elif option.strip() in '& 2 lisensi'.split():
     print()
     os.system('nano LICENSE')
@@ -72,7 +85,7 @@ elif option.strip() in '# 3 info'.split():
     print(example)
     os.system('toilet -f smslant Grab')
     print(info)
-    time.sleep(1)
+    sleep(1)
     print()
     input('[ Tekan Enter ]')
     restart()
@@ -80,14 +93,14 @@ elif option.strip() in '* 4 perbarui'.split():
     print()
     os.system('git pull origin master')
     print()
-    input('\033[0m[ \033[92mTekan Enter \033[0m]')
+    input('\033[0m[ \033[32mTekan Enter \033[0m]')
     restart()
-elif option.strip() in '- keluar 0'.split():
+elif option.strip() in '- 0 keluar'.split():
     print("\n\033[0m[\033[1;91m!\033[0m] \033[1;77mKeluar dari program!")
     print()
     sys.exit(1)
 else:
     print("\n\033[0m[=\033[1;41;77m Pilihan Salah \033[0m=]")
     print()
-    time.sleep(1)
+    sleep(1)
     restart()
